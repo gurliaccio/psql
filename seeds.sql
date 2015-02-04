24c89af37522787ef9f3fc23d99f8b94354e290c
Michael Gurliaccio <gurliaccio@gmail.com>
2015-02-04 09:25:45 -0500

    homework 1

---
+++ seeds.sql
@@ -0,0 +1,12 @@
+INSERT INTO shows (name, genre, rating, airing) VALUES 
+  ('Six Feet Under', 'Drama', 99, true),
+  ('Broad City', 'Comedy', 98, true),
+  ('Colbert Report', 'Comedy', 97, false),
+  ('PBS Newshour', 'News', 96, true),
+  ('The Wire', 'Drama', 99, false),
+  ('The Daily Show', 'News', 95, true),
+  ('Louie', 'Comedy', 98, true),
+  ('Booknotes', 'Educational', 94, true),
+  ('Strangers with Candy', 'Educational', 93, false);
+
+#select * from shows; to check status
